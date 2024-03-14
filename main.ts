fc7d6c331c3eb5a44d4cf768344a2a4aa6f9c627
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-14 22:26:29 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+// Q37
+// function make_shirt(size: string = "large",message: string = "I love TypeScript"){
+//     console.log(`Making a ${size} t-shirt with the message"${message}" printed on it.`)
+// }
+// make_shirt()
+// make_shirt("medium")
+// make_shirt("small","I love coding")
+// Q38
+// function describe_city(city: string , country: string = "Pakistan"){
+//     console.log(`${city} is in ${country}.`)
+//  }
+
+// describe_city("Karachi")
+// describe_city("Lahore")
+// describe_city("Tokyo","Japan")
+// Q39
+//  function city_country(city: string, country: string): string{
+//     return `${city} is a city of ${country}`
+//  }
+//  console.log(city_country("Lahore","Pakistan"))
+//  console.log(city_country("Tokyo","Japan"))
+//  console.log(city_country("Paris","France"))
